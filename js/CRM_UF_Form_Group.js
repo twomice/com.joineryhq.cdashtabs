175529b12981888a5498d09bbd481505a7cbe419
Allen Shaw <allen@JoineryHQ.com>
2020-12-22 17:40:08 -0600

    NFC: code cleanup on js files.

--- js/CRM_UF_Form_Group.js
+++ js/CRM_UF_Form_Group.js
@@ -1,9 +1,15 @@
 CRM.$(function($) {
+  // Add id attribute to bhfe table, so it's easy to reference later.
   CRM.$('input#is_cdash').closest('table').attr('id', 'bhfe-table');
+  // Move bhfe form elements into main form.
   CRM.$('tr.crm-ufgroup-form-block-uf_group_type').after(CRM.$('input#is_show_pre_post').closest('tr'));
   CRM.$('tr.crm-ufgroup-form-block-uf_group_type').after(CRM.$('input#is_cdash').closest('tr'));
+  // Remove bhfe table.
   CRM.$('table#bhfe-table').remove();
 
+  /**
+   * onChange handler for is_cdash checkbox.
+   */
   var isCdashChange = function isCdashChange() {
     if (CRM.$('input#is_cdash').is(':checked')) {
       CRM.$('input#is_show_pre_post').closest('tr').show();
