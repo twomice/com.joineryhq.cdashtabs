8aa120b821afa24b7caaddff7dce9be9f047c620
Allen Shaw <allen@JoineryHQ.com>
2023-07-06 11:57:18 -0500

    civix upgrade

--- CRM/Cdashtabs/Upgrader.php
+++ CRM/Cdashtabs/Upgrader.php
@@ -4,7 +4,7 @@ use CRM_Cdashtabs_ExtensionUtil as E;
 /**
  * Collection of upgrade steps.
  */
-class CRM_Cdashtabs_Upgrader extends CRM_Cdashtabs_Upgrader_Base {
+class CRM_Cdashtabs_Upgrader extends CRM_Extension_Upgrader_Base {
 
   // By convention, functions that look like "function upgrade_NNNN()" are
   // upgrade tasks. They are executed in order (like Drupal's hook_update_N).
--- cdashtabs.php
+++ cdashtabs.php
@@ -129,15 +129,6 @@ function cdashtabs_civicrm_config(&$config) {
 }
 
 /**
- * Implements hook_civicrm_xmlMenu().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_xmlMenu
- */
-function cdashtabs_civicrm_xmlMenu(&$files) {
-  _cdashtabs_civix_civicrm_xmlMenu($files);
-}
-
-/**
  * Implements hook_civicrm_install().
  *
  * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_install
@@ -147,114 +138,12 @@ function cdashtabs_civicrm_install() {
 }
 
 /**
- * Implements hook_civicrm_postInstall().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_postInstall
- */
-function cdashtabs_civicrm_postInstall() {
-  _cdashtabs_civix_civicrm_postInstall();
-}
-
-/**
- * Implements hook_civicrm_uninstall().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_uninstall
- */
-function cdashtabs_civicrm_uninstall() {
-  _cdashtabs_civix_civicrm_uninstall();
-}
-
-/**
  * Implements hook_civicrm_enable().
  *
  * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_enable
  */
 function cdashtabs_civicrm_enable() {
   _cdashtabs_civix_civicrm_enable();
-}
-
-/**
- * Implements hook_civicrm_disable().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_disable
- */
-function cdashtabs_civicrm_disable() {
-  _cdashtabs_civix_civicrm_disable();
-}
-
-/**
- * Implements hook_civicrm_upgrade().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_upgrade
- */
-function cdashtabs_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _cdashtabs_civix_civicrm_upgrade($op, $queue);
-}
-
-/**
- * Implements hook_civicrm_managed().
- *
- * Generate a list of entities to create/deactivate/delete when this module
- * is installed, disabled, uninstalled.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_managed
- */
-function cdashtabs_civicrm_managed(&$entities) {
-  _cdashtabs_civix_civicrm_managed($entities);
-}
-
-/**
- * Implements hook_civicrm_caseTypes().
- *
- * Generate a list of case-types.
- *
- * Note: This hook only runs in CiviCRM 4.4+.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_caseTypes
- */
-function cdashtabs_civicrm_caseTypes(&$caseTypes) {
-  _cdashtabs_civix_civicrm_caseTypes($caseTypes);
-}
-
-/**
- * Implements hook_civicrm_angularModules().
- *
- * Generate a list of Angular modules.
- *
- * Note: This hook only runs in CiviCRM 4.5+. It may
- * use features only available in v4.6+.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_angularModules
- */
-function cdashtabs_civicrm_angularModules(&$angularModules) {
-  _cdashtabs_civix_civicrm_angularModules($angularModules);
-}
-
-/**
- * Implements hook_civicrm_alterSettingsFolders().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_alterSettingsFolders
- */
-function cdashtabs_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
-  _cdashtabs_civix_civicrm_alterSettingsFolders($metaDataFolders);
-}
-
-/**
- * Implements hook_civicrm_entityTypes().
- *
- * Declare entity types provided by this module.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_entityTypes
- */
-function cdashtabs_civicrm_entityTypes(&$entityTypes) {
-  _cdashtabs_civix_civicrm_entityTypes($entityTypes);
-}
-
-/**
- * Implements hook_civicrm_thems().
- */
-function cdashtabs_civicrm_themes(&$themes) {
-  _cdashtabs_civix_civicrm_themes($themes);
 }
 
 // --- Functions below this ship commented out. Uncomment as required. ---
