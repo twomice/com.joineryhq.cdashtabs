8461e6a6a2e4dc24f229aacd3d78daccce5699ab
Allen Shaw <allen@JoineryHQ.com>
2026-03-24 18:11:03 -0500

    Make bhfe handling kinder to other extensions.

--- js/CRM_UF_Form_Group.js
+++ js/CRM_UF_Form_Group.js
@@ -1,13 +1,15 @@
 CRM.$(function($) {
   // Add id attribute to bhfe table, so it's easy to reference later.
-  CRM.$('input#cdashtabs_is_cdash').closest('table').attr('id', 'bhfe-table');
+  CRM.$('input#cdashtabs_is_cdash').closest('table').addClass('cdashtabs-bhfe-table');
   // Move bhfe form elements into main form.
   CRM.$('tr.crm-ufgroup-form-block-uf_group_type').after(CRM.$('input#cdashtabs_is_edit').closest('tr'));
   CRM.$('tr.crm-ufgroup-form-block-uf_group_type').after(CRM.$('input#cdashtabs_is_show_pre_post').closest('tr'));
   CRM.$('tr.crm-ufgroup-form-block-uf_group_type').after(CRM.$('select#cdashtabs_cdash_contact_type').closest('tr'));
   CRM.$('tr.crm-ufgroup-form-block-uf_group_type').after(CRM.$('input#cdashtabs_is_cdash').closest('tr'));
-  // Remove bhfe table.
-  // CRM.$('table#bhfe-table').remove();
+  // Remove the bhfe table, but only if it's empty.
+  if ($('table.cdashtabs-bhfe-table tr').length == 0) {
+    $('table.cdashtabs-bhfe-table').remove();
+  }
 
   /**
    * onChange handler for cdashtabs_is_cdash checkbox.
